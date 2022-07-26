856dd6f8f2ecee736720b2fc943309d2bed852a3
1047394922 <106104359+1047394922@users.noreply.github.com>
2022-07-25 20:37:51 -0500

    Add files via upload

---
+++ Principal/Main.java
@@ -0,0 +1,16 @@
+
+package Principal;
+import Vistas.*;
+
+public class Main {
+
+    
+    public static void main(String[] args) {
+        //Creamos la instancia de la vista login
+        Login login = new Login(); 
+        // El metodo setvisible hace visble la ventana 
+        login.setVisible(true);
+       
+    }
+    
+}
